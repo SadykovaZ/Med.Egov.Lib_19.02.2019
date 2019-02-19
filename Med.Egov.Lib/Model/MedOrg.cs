e0a87128a71068673fe868e17a5738a702a0b446
СадыковаЖ <@205_8>
2019-02-19 11:18:31 +0600

    ++

---
+++ Med.Egov.Lib/Model/MedOrg.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Med.Egov.Lib.Model
+{
+    public class MedOrg
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Patient> Patiens = null;
+
+        public MedOrg():this("","")
+        {
+                 
+        }
+        public MedOrg(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+            Patiens = new List<Patient>();
+        }
+
+
+
+
+    }
+}
--- Med.Egov.Lib/Model/Patient.cs
+++ Med.Egov.Lib/Model/Patient.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace Med.Egov.Lib.Model
 {
-    class Patient
+    public class Patient
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+
+        private string IIN;
+        public string Iin {
+            get
+            {
+                return IIN;
+            }
+            set
+            {
+                if (value.Length != 12)
+                {
+                    throw new ArgumentException("Введите корректный ИИН");
+                }
+                IIN = value;
+            }
+        }
+        public MedOrg MedOrg { get; set; } = null;
+
+
+
 
     }
 }
